8104a55e9f6d66fff77493ad053ec9574653334c
Alexander Moreno Delgado <alexander.moreno@gorillalogic.com>
2020-05-14 16:10:30 -0600

    [TECHOPS-1927] feature: added log policies to the ES cluster

--- main.tf
+++ main.tf
@@ -1,3 +1,36 @@
+
+# Log Management
+
+resource "aws_cloudwatch_log_group" "es_log_group" {
+  name = "${var.es_domain}-log_group"
+}
+
+resource "aws_cloudwatch_log_resource_policy" "es_log_resource_policy" {
+  policy_name = "${var.es_domain}-es_log_resource_policy"
+
+  policy_document = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "es.amazonaws.com"
+      },
+      "Action": [
+        "logs:PutLogEvents",
+        "logs:PutLogEventsBatch",
+        "logs:CreateLogStream"
+      ],
+      "Resource": "arn:aws:logs:*"
+    }
+  ]
+}
+EOF
+}
+
+# Cluster creation
+
 resource "aws_elasticsearch_domain" "es_domain" {
   domain_name           = var.es_domain
   elasticsearch_version = var.es_version
@@ -16,7 +49,13 @@ ebs_options {
     volume_size = (var.es_ebs_enabled == true ? var.es_volume_size : null)
 }
 
+  log_publishing_options {
+    cloudwatch_log_group_arn = "${aws_cloudwatch_log_group.es_log_group.arn}"
+    log_type                 = "INDEX_SLOW_LOGS"
+  }
+
   tags = {
-    ES_Domain = var.es_domain
+    es_domain = var.es_domain
+    es_created_by = "terraform-elasticsearch-action"
   }
 }
