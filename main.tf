a8ca7dfbf462850c5dde091fe37cae0b5d872165
Alexander Moreno Delgado <alexander.moreno@gorillalogic.com>
2020-05-14 17:05:51 -0600

    [TECHOPS-1927] Adding access policy from source IP

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
-
 # Log Management
+
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
 
 resource "aws_cloudwatch_log_group" "es_log_group" {
   name = "${var.es_domain}-log_group"
@@ -43,19 +46,36 @@ resource "aws_elasticsearch_domain" "es_domain" {
     automated_snapshot_start_hour = var.es_snapshot_hour
   }
 
-ebs_options {
+  ebs_options {
     ebs_enabled = var.es_ebs_enabled
     volume_type = (var.es_ebs_enabled == true ? var.es_volume_type : null)
     volume_size = (var.es_ebs_enabled == true ? var.es_volume_size : null)
+  }
+
+  access_policies = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "es:*",
+      "Principal": "*",
+      "Effect": "Allow",
+      "Resource": "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/${var.es_domain}/*",
+      "Condition": {
+        "IpAddress": {"aws:SourceIp": [${var.es_source_ip}]}
+      }
+    }
+  ]
 }
+EOF
 
   log_publishing_options {
-    cloudwatch_log_group_arn = "${aws_cloudwatch_log_group.es_log_group.arn}"
+    cloudwatch_log_group_arn = aws_cloudwatch_log_group.es_log_group.arn
     log_type                 = "INDEX_SLOW_LOGS"
   }
 
   tags = {
-    es_domain = var.es_domain
+    es_domain     = var.es_domain
     es_created_by = "terraform-elasticsearch-action"
   }
 }
